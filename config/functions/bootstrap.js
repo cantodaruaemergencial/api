d85e6f5cbd3dcdd2a71713cb26d3aa08059389b8
Matheus Araujo <matheus.saraujo@gmail.com>
2021-04-03 14:22:35 -0300

    config public routes

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,4 +10,38 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+const _ = require("lodash");
+
+const allowGetRoutes = async (controller) => {
+  const service = await strapi.plugins["users-permissions"].services.userspermissions;
+  const plugins = await service.getPlugins("en");
+  const roles = await service.getRoles();
+
+  const getRole = async (type) => {
+    const { id } = _.find(roles, x => x.type === type);
+    return service.getRole(id, plugins);
+  }
+
+  const setPermission = (role, type, controller, action, enabled) => {
+    try {
+      role.permissions[type].controllers[controller][action].enabled = enabled;
+    }
+    catch (e) {
+      console.error(`Couldn't set permission ${role.name} ${type}:${controller}:${action}:${enabled}`);
+    }
+  }
+
+  const publicRole = await getRole("public");
+  setPermission(publicRole, "application", controller, "count", true);
+  setPermission(publicRole, "application", controller, "find", true);
+  setPermission(publicRole, "application", controller, "findone", true);
+  await service.updateRole(publicRole.id, publicRole);
+}
+
+module.exports = async () => {
+
+  allowGetRoutes('benefit');
+  allowGetRoutes('external-service');
+
+  return;
+};
