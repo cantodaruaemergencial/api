64098f1f39849c5ec1cd6a1384157625d80eb67f
Matheus Araujo <matheus.saraujo@gmail.com>
2021-05-17 20:33:21 -0300

    creating route person2

--- api/person/controllers/person.js
+++ api/person/controllers/person.js
@@ -21,12 +21,12 @@ module.exports = {
         "pe.Date as TodayEntranceTime, " +
         "(select count(1) from person_entrances pe1 where pe1.person = p.id) as Entrances " +
         "from people p left join person_entrances pe on p.id = pe.person and date(pe.date) = date(now()) " +
-        "where :filter = '' or (Name like :filter or SocialName like :filter or CardNumber like :filter) " +
+        "where Name like :filter or SocialName like :filter or CardNumber like :filter " +
         "order by p.Name " +
         "limit :limit offset :offset;",
       params
     );
-    
+
     ctx.send(result[0]);
   },
 };
