badc9ca77e1e32697cdf2c39fe83a153528ec66a
Matheus Araujo <matheus.saraujo@gmail.com>
2021-05-18 08:54:59 -0300

    changes on /person2

--- api/person/controllers/person.js
+++ api/person/controllers/person.js
@@ -9,30 +9,19 @@ module.exports = {
   async find2(ctx) {
     const knex = strapi.connections.default;
 
-    const q = ctx.query
-      ? ctx.query
-      : {
-          limit: 7,
-          offset: 0,
-          filter: "",
-        };
-
-    if (!q.limit) q.limit = 7;
-    if (!q.offset) q.offset = 0;
-    if (!q.filter) q.filter = "";
-
     const params = {
-      limit: Number(q.limit),
-      offset: Number(q.start),
-      filter: "%" + q.filter + "%",
+      limit: Number(ctx.query.limit),
+      offset: Number(ctx.query.start),
+      filter: ctx.query.filter ? "%" + ctx.query.filter + "%" : '%'
     };
 
     const result = await knex.raw(
       "select " +
         "p.Id, p.Name, p.SocialName, p.CardNumber, " +
-        "pe.DateTime as TodayEntranceTime, " +
-        "(select count(1) from person_entrances pe1 where pe1.person = p.id) as Entrances " +
-        "from people p left join person_entrances pe on p.id = pe.person and date(pe.DateTime) = date(now()) " +
+        "(select pe1.datetime from person_entrances pe1 where pe1.person = p.id order by pe1.datetime desc limit 1) as LastEntranceDate, " +
+        "(select count(1) from person_entrances pe2 where pe2.person = p.id and date(datetime) = date(now())) as EnteredToday, " +
+        "(select count(1) from person_entrances pe3 where pe3.person = p.id) as Entrances " +
+        "from people p " +
         "where Name like :filter or SocialName like :filter or CardNumber like :filter " +
         "order by p.Name " +
         "limit :limit offset :offset;",
